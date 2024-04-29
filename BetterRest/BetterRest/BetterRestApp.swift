568957226b964168a9e080d454c1e2e502f9cdd1
aqeel-01 <133170261+aqeel-01@users.noreply.github.com>
2024-04-29 11:44:41 +0500

    Better Rest app
    (which recommends the perfect time to sleep based on your current status)

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by aqeel on 16/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
